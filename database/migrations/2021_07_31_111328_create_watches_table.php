85a0b263d3c26d15017fb1c3231b0ca4d13ac82b
Mirza Krupic <krupicmirza99@gmail.com>
2021-07-31 13:47:05 +0200

    add watches table and model

---
+++ database/migrations/2021_07_31_111328_create_watches_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWatchesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('watches', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('image_url')->default('https://hugyourlife.eu/wp-content/uploads/2020/06/placeholder.png');
+            $table->string('name');
+            $table->decimal('price', 5, 2);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('watches');
+    }
+}
